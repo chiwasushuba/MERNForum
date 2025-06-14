441689318dd7ed6f9d06776dadc417b168ed5eba
chiwasushuba <joshua11084@gmail.com>
2025-06-15 00:25:39 +0800

    refactor: changed the sending and getting message to the logged person instead

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -4,27 +4,31 @@ const Conversation = require('../models/conversationModel');
 // Send a message
 const sendMessage = async (req, res) => {
   try {
-    const { senderId, receiverId, content } = req.body;
+    const senderId = req.user._id;
+    const { receiverId, content } = req.body;
 
-    // Create and save the message
     const newMessage = await Message.create({
       content,
       senderId,
       receiverId,
     });
 
-    // Create or update the conversation
-    await Conversation.findOneAndUpdate(
-      {
-        members: { $all: [senderId, receiverId] }
-      },
-      {
+    // Update or create the conversation
+    const existingConversation = await Conversation.findOne({
+      members: { $all: [senderId, receiverId] },
+    });
+
+    if (existingConversation) {
+      existingConversation.lastMessage = content;
+      existingConversation.lastMessageAt = new Date();
+      await existingConversation.save();
+    } else {
+      await Conversation.create({
         members: [senderId, receiverId],
         lastMessage: content,
-        lastMessageAt: Date.now()
-      },
-      { upsert: true, new: true }
-    );
+        lastMessageAt: new Date(),
+      });
+    }
 
     res.status(201).json(newMessage);
   } catch (err) {
@@ -33,10 +37,12 @@ const sendMessage = async (req, res) => {
   }
 };
 
+
 // Get all messages between two users
 const getMessages = async (req, res) => {
   try {
-    const { userId1, userId2 } = req.params;
+    const userId1 = req.user._id;
+    const userId2 = req.params.userId; // use req.params.userId instead of destructuring
 
     const messages = await Message.find({
       $or: [
@@ -52,6 +58,7 @@ const getMessages = async (req, res) => {
   }
 };
 
+
 // Mark message as read
 const markAsRead = async (req, res) => {
   try {
@@ -63,46 +70,45 @@ const markAsRead = async (req, res) => {
   }
 };
 
-//
-
 // Soft delete message
 const softDeleteMessage = async (req, res) => {
   try {
     const { messageId } = req.params;
-    await Message.findByIdAndUpdate(messageId, { isDeleted: true });
-    res.json({ success: true });
+    const updated = await Message.findByIdAndUpdate(messageId, { isDeleted: true }, { new: true });
+    res.json({ success: true, data: updated });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
 // Edit a message
+const updateMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+    const { newContent } = req.body;
 
-const updateMessage = async (req,res) => {
-  try{
-    const {messageId} = req.params;
-    const {newContent} = req.body;
-
-    const updated = await Message.findByIdAndUpdate(messageId, {content: newContent})
+    const updated = await Message.findByIdAndUpdate(
+      messageId,
+      { content: newContent },
+      { new: true }
+    );
     res.status(200).json(updated);
-
-  } catch (err){
-    res.status(500).json({error: err.message});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
+};
 
-const deleteMessage = async (req,res) => {
-  try{
-    const {messageId} = req.params;
+// Hard delete message
+const deleteMessage = async (req, res) => {
+  try {
+    const { messageId } = req.params;
 
     const message = await Message.findByIdAndDelete(messageId);
-    res.status(200).json({message: `deleted${message}`})
-
-  } catch (err){
-    res.status(500).json({error: err.message});
+    res.status(200).json({ message: 'Message deleted successfully', data: message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
-
+};
 
 module.exports = {
   sendMessage,
@@ -111,5 +117,4 @@ module.exports = {
   softDeleteMessage,
   updateMessage,
   deleteMessage,
-
-}
+};
