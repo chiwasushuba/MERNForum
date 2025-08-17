d3ff971e0597e5a6ade589500f5e805b0dbf57d6
chiwasushuba <joshua11084@gmail.com>
2025-08-17 13:51:41 +0800

    fix: fixed conversationModel to better handle groups

--- backend/models/conversationModel.js
+++ backend/models/conversationModel.js
@@ -1,16 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const conversationSchema = new Schema(
   {
-    members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    lastMessage: { type: String },
+    members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    isGroup: { type: Boolean, default: false },
+    groupName: { type: String },
+    groupAvatar: { type: String },
+
+    lastMessage: { type: Schema.Types.ObjectId, ref: "Message" },
     lastMessageAt: { type: Date, default: Date.now },
+
+    unreadCounts: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        count: { type: Number, default: 0 },
+      },
+    ],
   },
   { timestamps: true }
 );
 
-// Index for quick querying
+// Helpful indexes
 conversationSchema.index({ members: 1 });
+conversationSchema.index({ lastMessageAt: -1 });
 
-module.exports = mongoose.model('Conversation', conversationSchema);
+module.exports = mongoose.model("Conversation", conversationSchema);
