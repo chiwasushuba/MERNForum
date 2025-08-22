5ce81e5c5d1ccb6076cf2755d165f4dc94a2e997
chiwasushuba <joshua11084@gmail.com>
2025-08-23 00:45:15 +0800

    fix: fixed socket.io in frontend

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -23,7 +23,7 @@ export default function ChatPage() {
   useEffect(() => {
     if (!authIsReady || !userInfo) return;
 
-    const s = io('http://localhost:4000', {
+    const s = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
       transports: ['websocket'],
     });
 
