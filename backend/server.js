1081b987fb441526bc6a17be23d359c872490425
chiwasushuba <joshua11084@gmail.com>
2025-08-23 00:38:49 +0800

    fix: fixed cors process.env

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 // Socket.IO setup
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST']
   }
 });
